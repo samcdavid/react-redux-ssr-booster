0e93b22b277887a391c03f865a362f063b4bca95
Sam McDavid <mcdavidsa@gmail.com>
2016-10-07 12:15:46 -0400

    Setup React Router
    Why
    ---
    
    - Any app worth its salt is going to need multiple routes to handle
    different presentations.
    
    This change addresses the need by
    ---------------------------------
    
    - Install react router.
    
    Next Steps
    ----------
    
    - Setup a redux app.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+import App from './routes/App.js'
+import './index.css'
 
 ReactDOM.render(
-  <App />,
+  <Router
+    history={ browserHistory }
+    routes={ App }
+  />,
   document.getElementById('root')
-);
+)
---
+++ src/routes/App.js
@@ -0,0 +1,8 @@
+import component from '../views/App'
+
+export const App = {
+  path: '/',
+  component
+}
+
+export default App
